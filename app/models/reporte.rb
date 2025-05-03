6377aa7afe7b2be2e8f07db5986faa21b0412784
alectrico <ventas@alectrico.cl>
2025-05-03 08:02:20 -0400

    La validación de reportes

--- app/models/reporte.rb
+++ app/models/reporte.rb
@@ -2,8 +2,8 @@ class Reporte < ApplicationRecord
   has_many :comentarios, dependent: :destroy
   has_rich_text :contenido
 
-  #validates_presence_of :nombre
-  #validates :nombre, length: { in: 2..25}
+  validates_presence_of :nombre
+  validates :nombre, length: { in: 2..25}
 
   validates_presence_of :fono
   validates :fono, length: { in: 9..9}
