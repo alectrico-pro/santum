74861b6d19092376c109a462588e411f1a5dbe0a
alectrico <ventas@alectrico.cl>
2025-05-06 10:08:38 -0400

    Denuevo con todas las imágenes

--- app/models/graph/solicitud/component/image.rb
+++ app/models/graph/solicitud/component/image.rb
@@ -1,16 +1,18 @@
 module Graph
   module Solicitud
-    class Image  < Component
-      attr_accessor :type
-      attr_accessor :image
-      def initialize(image=nil)
-        @type  = "image"
-        @image = image
-      end
+    class Component
+      class ::Graph::Solicitud::Component::Image  < ::Graph::Solicitud::Component
+        attr_accessor :type
+        attr_accessor :image
+        def initialize(image=nil)
+          @type  = "image"
+          @image = image
+        end
 
-      def self.json_create(o)
-        attrs = JSON.parse(o['data'])
-        Solicitud::Language.new(attrs['code'])
+        def self.json_create(o)
+          attrs = JSON.parse(o['data'])
+          Solicitud::Language.new(attrs['code'])
+        end
       end
     end
   end
--- app/models/graph/solicitud/component/text.rb
+++ app/models/graph/solicitud/component/text.rb
@@ -1,16 +1,18 @@
 module Graph
   module Solicitud
-    class Text  < Component
-      attr_accessor :type
-      attr_accessor :text
-      def initialize(text='')
-        @type  = "text"
-        @image = text
-      end
+    class Component
+      class ::Graph::Solicitud::Component::Text  < ::Graph::Solicitud::Component
+        attr_accessor :type
+        attr_accessor :text
+        def initialize(text='')
+          @type  = "text"
+          @image = text
+        end
 
-      def self.json_create(o)
-        attrs = JSON.parse(o['data'])
-        Solicitud::Language.new(attrs['code'])
+        def self.json_create(o)
+          attrs = JSON.parse(o['data'])
+          Solicitud::Language.new(attrs['code'])
+        end
       end
     end
   end
