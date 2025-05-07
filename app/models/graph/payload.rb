b174cbf620cdf390008beae7a4a0b19dacbb0985
alectrico <ventas@alectrico.cl>
2025-05-07 05:59:54 -0400

    Sin responder

--- app/models/graph/payload.rb
+++ app/models/graph/payload.rb
@@ -37,8 +37,6 @@ module Graph
                   mensaje = "Hemos confirmado su número telefónico."
                   linea.warn mensaje
   #                ::Waba::Transaccion.new(:cliente).responder( @fono, @contexto.id, mensaje)
-                   ::Waba::Transaccion.new(:cliente).set_read_to( @contexto.id)
- #                 ::Waba::Transaccion.new(:cliente).set_delivered_to( @contexto.id)
                    #mensaje = "Ud. ha reportado #{reporte.contenido}"
                    #::Waba::Transaccion.new(:cliente).enviar_mensaje( @fono, mensaje)
 
