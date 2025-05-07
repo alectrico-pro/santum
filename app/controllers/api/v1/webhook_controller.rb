26d972beb95571f3beee99ccabe88199d3fc6d92
alectrico <ventas@alectrico.cl>
2025-05-07 07:05:22 -0400

    Deneuvo

--- app/controllers/api/v1/webhook_controller.rb
+++ app/controllers/api/v1/webhook_controller.rb
@@ -1,77 +1,63 @@
- module Api
-    module V1
-      class WebhookController < Api::V1Controller
+module Api
+  module V1
+    class WebhookController < Api::V1Controller
 
-        include Linea
-        include Biblioteca::Mensajeria
-        include CrearUsuario
-
-
-        def verifica params
-          linea.info "En verifica"
-	  linea.info waba_params.inspect
-          es_challenge = false
-          modo      = waba_params['hub.mode']
-          challenge = waba_params['hub.challenge']
-          token     = waba_params['hub.verify_token']
-          linea.info "Token a verificar es:"
-          linea.info token
-
-          linea.info "Token en Waba"
-          linea.info WabaCfg.token_de_verificacion
+      include Linea
+      include Biblioteca::Mensajeria
+      include CrearUsuario
 
 
-          if modo == 'subscribe' and token == WabaCfg.token_de_verificacion.to_s
-            linea.info "Es challenge"
-            linea.info "Se devolverá"
-            linea.info challenge
-            es_challenge = true
-            render plain:  challenge , status: :ok  
-          else
-            linea.info "No es challenge"
-            render json: { :Error => "El acceso no está permitido!" }, status: :not_found  
-          end
+      def verifica params
+        linea.info "En verifica"
+        linea.info waba_params.inspect
+        es_challenge = false
+        modo      = waba_params['hub.mode']
+        challenge = waba_params['hub.challenge']
+        token     = waba_params['hub.verify_token']
+        linea.info "Token a verificar es:"
+        linea.info token
+
+        linea.info "Token en Waba"
+        linea.info WabaCfg.token_de_verificacion
+
+
+        if modo == 'subscribe' and token == WabaCfg.token_de_verificacion.to_s
+          linea.info "Es challenge"
+          linea.info "Se devolverá"
+          linea.info challenge
+          es_challenge = true
+          render plain:  challenge , status: :ok  
+        else
+          linea.info "No es challenge"
+          render json: { :Error => "El acceso no está permitido!" }, status: :not_found  
         end
+      end
 
-        #endpoint para flows con contenido dinámico
-        def flows
-          linea.warn "En flow"
-          linea.info "waba_params es:"
-          linea.info waba_params.inspect
-          ::Waba::Flow.new( presupuesto_params )
+      def webhook
+        linea.warn "En webhook"
+        linea.info "waba_params es:"
+        linea.info waba_params.inspect
+        if presupuesto_params['hub.mode'] == 'subscribe'
+          verifica presupuesto_params
+          return
+        else
+          #Tests if render or redirect has already happened.
+          ::Waba::Webhook.new( presupuesto_params )
           unless performed?
             respond_to do |format|
               format.json { render json: { :status => :ok  } }
             end
-          end 
-        end
-
-        def webhook
-          linea.warn "En webhook"
-	  linea.info "waba_params es:"
-	  linea.info waba_params.inspect
-          if presupuesto_params['hub.mode'] == 'subscribe'
-            verifica presupuesto_params
             return
-          else
-            #Tests if render or redirect has already happened.
-            ::Waba::Webhook.new( presupuesto_params )
-            unless performed?
-              respond_to do |format|
-                format.json { render json: { :status => :ok  } }
-              end
-              return
-            end
-          end 
-        end
+          end
+        end 
+      end
 
 
-	private
+      private
 
-        def presupuesto_params
-          params.permit!
-        end
-
+      def presupuesto_params
+        params.permit!
       end
     end
   end
+end
