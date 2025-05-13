aa0a681561ed0c30c56528a1098ca379425209c4
alectrico <ventas@alectrico.cl>
2025-05-13 11:43:34 -0400

    Sin sleep 3

--- Gemfile
+++ Gemfile
@@ -94,3 +94,5 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+gem "simple_form", "~> 5.1.0"
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action -> { sleep 3 } #prueba para ver el progress bar, eliminar posteriormente
+#  before_action -> { sleep 3 } #prueba para ver el progress bar, eliminar posteriormente
 end
